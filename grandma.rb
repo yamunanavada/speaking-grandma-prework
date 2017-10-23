5cb549f5c2182cbc7476f9d8d9ff947f77ca8b4a
Yamuna Navada <yamunanavada@gmail.com>
2017-10-23 18:26:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
      "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-     "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   end
 end
